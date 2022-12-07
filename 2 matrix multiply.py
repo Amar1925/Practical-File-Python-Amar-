c90256b87c2e03bf686b4bcee1c7f53e477d1eb0
AmarKumar1912 <119916402+AmarKumar1912@users.noreply.github.com>
2022-12-07 11:01:06 +0530

    Update 2 matrix multiply.py

--- 2 matrix multiply.py
+++ 2 matrix multiply.py
@@ -1,29 +1,12 @@
-# Program to multiply two matrices using nested loops
-
-# take a 3x3 matrix
-A = [[12, 7, 3],
-	[4, 5, 6],
-	[7, 8, 9]]
-
-# take a 3x4 matrix
-B = [[5, 8, 1, 2],
-	[6, 7, 3, 0],
-	[4, 5, 9, 1]]
-	
-result = [[0, 0, 0, 0],
-		[0, 0, 0, 0],
-		[0, 0, 0, 0]]
-
-# iterating by row of A
-for i in range(len(A)):
-
-	# iterating by column by B
-	for j in range(len(B[0])):
-
-		# iterating by rows of B
-		for k in range(len(B)):
-			result[i][j] += A[i][k] * B[k][j]
-
-for r in result:
-	print(r)
+import numpy as np
  
+# input two matrices
+mat1 = ([1, 6, 5],[3 ,4, 8],[2, 12, 3])
+mat2 = ([3, 4, 6],[5, 6, 7],[6,56, 7])
+ 
+# This will return dot product
+res = np.dot(mat1,mat2)
+ 
+ 
+# print resulted matrix
+print(res)
